0dcbc32dc6f943eb3bc6e1e4fb908e2132a12327
NoNameGivenCoder <chaytontimedis@gmail.com>
2023-12-21 17:56:45 -0700

    added community showing

--- routes/ui/menu/titles.js
+++ routes/ui/menu/titles.js
@@ -3,9 +3,19 @@ const route = express.Router();
 const xmlbuilder = require('xmlbuilder');
 const moment = require('moment');
 
-route.get('/show', (req, res) => {
+const util = require('util')
+
+const con = require('../../../database_con');
+const query = util.promisify(con.query).bind(con)
+
+route.get('/show', async (req, res) => {
+
+    //TODO : hopefully adding in popular communities??
+    const newest_communities = await query(`SELECT * FROM communities ORDER BY create_time ASC`)
+
     res.render('pages/show', {
-        account : req.account[0]
+        account : req.account[0],
+        newest_communities : newest_communities
     })
 })
 
