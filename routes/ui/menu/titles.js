98a4b97116051e3ca62eee41de4bc71e792e3311
NoNameGivenCoder <chaytontimedis@gmail.com>
2023-12-21 18:44:51 -0700

    fixed newest communities query

--- routes/ui/menu/titles.js
+++ routes/ui/menu/titles.js
@@ -11,7 +11,7 @@ const query = util.promisify(con.query).bind(con)
 route.get('/show', async (req, res) => {
 
     //TODO : hopefully adding in popular communities??
-    const newest_communities = await query(`SELECT * FROM communities ORDER BY create_time ASC`)
+    const newest_communities = await query(`SELECT * FROM communities ORDER BY create_time DESC`)
 
     res.render('pages/show', {
         account : req.account[0],
